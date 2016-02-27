7b536ffcf98bf382da2eb5682e234558a85dc375
tas12 <tsultanah89gmail.comgit config --global user.name tasnimgit config --global user.email tsultanah89gmail.com>
2016-02-27 14:55:38 +0000

    handled endpoint for posted data

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
+var quesrystring = require('querystring');
 
 var server = http.createServer(handler);
 
 function handler(req, res){
   var endpoint = req.url;
+  var allTheData = '';
+
 
   if (endpoint === '/') {
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -14,29 +17,44 @@ function handler(req, res){
         console.log(err);
         return;
       }
-        res.end(file);
+      res.end(file);
 
     });
+  } else  if (endpoint === '/create-post') {
+
+    req.on('data', function(chunkOfData){
+      allTheData += chunkOfData;
+    });
+
+    req.on('end', function(){
+      console.log(quesrystring.parse(allTheData));
+      res.end();
+    });
+    res.writeHead(301, {"Location": "/"});
+    res.end();
   } else {
-  var ext = endpoint.split(".")[1];
+    var ext = endpoint.split(".")[1];
 
-  if (ext === "css" ) {
-    res.writeHead(200, {"Content-Type": "text/css"});
-  } else if (ext === "jpg") {
-    res.writeHead(200, {"Content-Type": "image/jpg"});
+    if (ext === "css" ) {
+      res.writeHead(200, {"Content-Type": "text/css"});
+    } else if (ext === "jpg") {
+      res.writeHead(200, {"Content-Type": "image/jpg"});
+    }
+
+    fs.readFile(__dirname + "/public" + endpoint, function(err, file){
+      if (err) {
+        console.log(err);
+
+      }
+      res.end(file);
+    });
   }
 
-  fs.readFile(__dirname + "/public" + endpoint, function(err, file){
-    if (err) {
-      console.log(err);
+
 
   }
 
-res.end(file);
-});
-}}
 
-
-server.listen(port, function(){
-  console.log("Server is listening on port" + port + ".  Ready to accept requests!");
-});
+  server.listen(port, function(){
+    console.log("Server is listening on port" + port + ".  Ready to accept requests!");
+  });
