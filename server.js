cba0784ec12c0bf4b8bd73a6d0603ea4816fbb07
tas12 <tsultanah89gmail.comgit config --global user.name tasnimgit config --global user.email tsultanah89gmail.com>
2016-02-27 12:34:47 +0000

    served html file

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var http = require('http');
-
+var fs = require('fs');
 var port = process.env.PORT || 3000;
-var message = "so happy to be at nodegirls";
-
-function handler(req, res){
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write(message);
-  res.end();
-}
 
 var server = http.createServer(handler);
+
+function handler(req, res){
+  var endpoint = req.url;
+
+  if (endpoint === '/') {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile(__dirname + "/public/index.html", function(err, file){
+      if(err){
+        console.log(err);
+        return;
+      }
+        res.end(file);
+
+    });
+  }
+
+}
+
 
 server.listen(port, function(){
   console.log("Server is listening on port" + port + ".  Ready to accept requests!");
