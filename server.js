d363bd8fe9a1089cd8de435e9741c964c76fb920
tas12 <tsultanah89gmail.comgit config --global user.name tasnimgit config --global user.email tsultanah89gmail.com>
2016-02-27 12:05:40 +0000

    added handler function

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
-var server = http.createServer();
+
 var port = process.env.PORT || 3000;
+var message = "so happy to be at nodegirls";
+
+function handler(req, res){
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(message);
+  res.end();
+}
+
+var server = http.createServer(handler);
 
 server.listen(port, function(){
   console.log("Server is listening on port" + port + ".  Ready to accept requests!");
