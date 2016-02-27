5bf2237e636099afa6848526c4174aa7c52ae839
tas12 <tsultanah89gmail.comgit config --global user.name tasnimgit config --global user.email tsultanah89gmail.com>
2016-02-27 15:27:21 +0000

    modularised handler

--- server.js
+++ server.js
@@ -1,58 +1,10 @@
 var http = require('http');
-var fs = require('fs');
+var handler = require('./src/handler.js');
+
 var port = process.env.PORT || 3000;
-var quesrystring = require('querystring');
+
 
 var server = http.createServer(handler);
-
-function handler(req, res){
-  var endpoint = req.url;
-  var allTheData = '';
-
-
-  if (endpoint === '/') {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile(__dirname + "/public/index.html", function(err, file){
-      if(err){
-        console.log(err);
-        return;
-      }
-      res.end(file);
-
-    });
-  } else  if (endpoint === '/create-post') {
-
-    req.on('data', function(chunkOfData){
-      allTheData += chunkOfData;
-    });
-
-    req.on('end', function(){
-      console.log(quesrystring.parse(allTheData));
-      res.end();
-    });
-    res.writeHead(301, {"Location": "/"});
-    res.end();
-  } else {
-    var ext = endpoint.split(".")[1];
-
-    if (ext === "css" ) {
-      res.writeHead(200, {"Content-Type": "text/css"});
-    } else if (ext === "jpg") {
-      res.writeHead(200, {"Content-Type": "image/jpg"});
-    }
-
-    fs.readFile(__dirname + "/public" + endpoint, function(err, file){
-      if (err) {
-        console.log(err);
-
-      }
-      res.end(file);
-    });
-  }
-
-
-
-  }
 
 
   server.listen(port, function(){
